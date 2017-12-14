5cf0f750f76aafc369d32442a2b788a529e0fd38
pranav kumar <kumaarpranv@gmail.com>
2017-12-14 15:33:40 +0530

    Update header.php

--- header.php
+++ header.php
@@ -56,7 +56,7 @@ if ($client->getAccessToken()) {
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="css/styles.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
   
 <style>
